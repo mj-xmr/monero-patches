9e21881c6c26360475cd75ae3a9a93a9761dd8e1
mj-xmr <63722585+mj-xmr@users.noreply.github.com>
2022-06-03 05:56:33 +0200

    Mac memory usage

--- src/monero-test-build-time.sh
+++ src/monero-test-build-time.sh
@@ -82,7 +82,8 @@ free_mem () {
 if [ "$(uname)" == "Linux" ]; then
 	free -g
 else
-	sysctl -n hw.memsize
+	vm_stat | perl -ne '/page size of (\d+)/ and $size=$1; /Pages\s+([^:]+)[^\d]+(\d+)/ and printf("%-16s % 16.2f Mi\n", "$1:", $2 * $size / 1048576);'
+	sysctl vm.swapusage
 fi	
 }
 
