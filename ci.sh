5618f6cbd4ca508e65f7ad2c0bf0149058b9f37e
mj-xmr <63722585+mj-xmr@users.noreply.github.com>
2022-09-23 12:32:37 +0200

    Testing in /tmp

--- ci.sh
+++ ci.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 DIR_THIS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-WDIR=monero-patches-build
+WDIR=/tmp/monero/monero-patches-build
 MONERO_DIR=monero-master
 mkdir -p $WDIR && cd $WDIR
 
